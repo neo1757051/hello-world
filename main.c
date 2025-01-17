85576b09c2a9eea6b953eda0ea7baa968a663f43
neo1757051 <rajiv.iyer100@gmail.com>
2025-01-17 14:36:02 -0800

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,12 @@ int main(){
   int i;
   int a[10] = 0;
 
-  printf("hellow world\n");
+  printf("hello world\n");
   //initialize fibonacci series
   a[0] = a[1] = 1;
-  printf("The first 10 numbers in the series are:\n);
+  printf("The first 10 numbers in the Fibonacci series are:\n);
   printf("%d\n%d\n", a[0], a[1]) ;
-  for (i=3:i<=9;i++)
+  for (i=3;i<=9;i++)
   {
     a[i]=a[i-1]+a[i-2];
     printf("a[i]\n");
